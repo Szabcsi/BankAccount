54263de8d1e51e6bb066e0136b069e4f060cda29
Szabcsi <beko.szabolcs@gmail.com>
2016-08-24 00:52:34 +0200

    My first commit

---
+++ src/main/java/hu/bank/BankAccount.java
@@ -0,0 +1,20 @@
+package hu.bank;
+
+
+public class BankAccount {
+	private double balance;
+
+	public BankAccount(long balance) {
+		this.balance = balance;
+	}
+
+	public double debit(double amount) {
+		if (balance <= amount) {
+			amount = balance;
+		}
+
+		balance -= amount;
+		return amount;
+	}
+
+}
---
+++ src/test/java/hu/bank/TestBankAccount.java
@@ -0,0 +1,14 @@
+package hu.bank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBankAccount {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
