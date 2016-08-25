a0a5f41e41d010ff15c0e58dde0216072b18a64a
Szabcsi <beko.szabolcs@gmail.com>
2016-08-25 23:32:15 +0200

    Adds jacoco plugin and test

--- src/test/java/hu/bank/TestBankAccount.java
+++ src/test/java/hu/bank/TestBankAccount.java
@@ -1,14 +1,18 @@
 package hu.bank;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 public class TestBankAccount {
 
 	@Test
-	public void test() {
-		
+	public void testDebitWithSufficientFounds() {
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(5);
+		Assert.assertEquals(5.0, amount);
+
 	}
 
 }
